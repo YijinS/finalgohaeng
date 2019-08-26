a9677d2431931304ddd39fddd59c67300b7c94c7
JH <JH@DESKTOP-MSHE60B>
2019-08-26 09:58:52 +0900

    엑셀다운 예시

--- src/main/java/com/jscb/gohaeng/gameresult/service/GameResultServiceImpl.java
+++ src/main/java/com/jscb/gohaeng/gameresult/service/GameResultServiceImpl.java
@@ -1,8 +1,15 @@
 package com.jscb.gohaeng.gameresult.service;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor.HSSFColorPredefined;
+import org.apache.poi.ss.usermodel.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -134,6 +141,90 @@ public Map<String,Object> getColorByNumber(int start,int end) {
 		
 		return list;
 	}
+
+	@Override
+	public void getExelDown(HttpServletResponse response,int start, int end) {
+		List<LottoGamesDto> list = lottoGamesDao.getList(start, end);
+		//Excel Down 시작
+        Workbook workbook = new HSSFWorkbook();
+        //시트생성
+        Sheet sheet = workbook.createSheet("게시판");
+        
+        //행, 열, 열번호
+        Row row = null;
+        Cell cell = null;
+        int rowNo = 0;
+        
+        // 테이블 헤더용 스타일
+        CellStyle headStyle = workbook.createCellStyle();
+
+        // 가는 경계선을 가집니다.
+        headStyle.setBorderTop(BorderStyle.THIN);
+        headStyle.setBorderBottom(BorderStyle.THIN);
+        headStyle.setBorderLeft(BorderStyle.THIN);
+        headStyle.setBorderRight(BorderStyle.THIN);
+
+        // 배경색은 노란색입니다.
+        headStyle.setFillForegroundColor(HSSFColorPredefined.YELLOW.getIndex());
+        headStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        // 데이터는 가운데 정렬합니다.
+        headStyle.setAlignment(HorizontalAlignment.CENTER);
+
+        // 데이터용 경계 스타일 테두리만 지정
+        CellStyle bodyStyle = workbook.createCellStyle();
+        bodyStyle.setBorderTop(BorderStyle.THIN);
+        bodyStyle.setBorderBottom(BorderStyle.THIN);
+        bodyStyle.setBorderLeft(BorderStyle.THIN);
+        bodyStyle.setBorderRight(BorderStyle.THIN);
+
+        // 헤더 생성
+        row = sheet.createRow(rowNo++);
+
+        cell = row.createCell(0);
+        cell.setCellStyle(headStyle);
+        cell.setCellValue("회차");
+
+        cell = row.createCell(1);
+        cell.setCellStyle(headStyle);
+        cell.setCellValue("당첨번호");
+
+        cell = row.createCell(2);
+        cell.setCellStyle(headStyle);
+        cell.setCellValue("추첨일");
+
+        // 데이터 부분 생성
+        for(LottoGamesDto lotto : list) {
+            
+            row = sheet.createRow(rowNo++);
+            cell = row.createCell(0);
+            cell.setCellStyle(bodyStyle);
+            cell.setCellValue(""+lotto.getGames());
+            cell = row.createCell(1);
+            cell.setCellStyle(bodyStyle);
+            cell.setCellValue(""+lotto.getWinningNum());
+            cell = row.createCell(2);
+            cell.setCellStyle(bodyStyle);
+            cell.setCellValue(""+lotto.getDrawDate());
+        }
+
+        // 컨텐츠 타입과 파일명 지정
+        response.setContentType("ms-vnd/excel");
+        response.setHeader("Content-Disposition", "attachment;filename=test.xls");
+
+        // 엑셀 출력
+        try {
+			workbook.write(response.getOutputStream());
+			workbook.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+       
+
+	}
+
+
 
 	
 
